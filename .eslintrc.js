1a91d6828ecbcfe72fb22ddd70d17b0838e73078
rgled002 <rgled002@gold.ac.uk>
2021-12-16 13:19:12 +0000

    Project setup

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-typescript',
+  ],
+};
